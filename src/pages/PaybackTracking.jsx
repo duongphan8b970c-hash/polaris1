d14c525d6d62ff0e09d438b37042ebfed19f0216
duongphan8b970c-hash <duong.phan8b970c@gmail.com>
2026-02-04 08:47:08 +0000

    Fix table size

--- src/pages/PaybackTracking.jsx
+++ src/pages/PaybackTracking.jsx
@@ -150,15 +150,6 @@ export default function PaybackTracking() {
         </div>
       )}
 
-      {/* Info Box */}
-      <div className="bg-blue-50 border border-blue-200 rounded-lg p-4 mb-6">
-        <div className="flex items-start gap-3">
-          <svg className="w-5 h-5 text-blue-500 flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-        </div>
-      </div>
-
       {/* Goals List */}
       <PaybackGoalList
         goals={goals}
