daaf842b0b10d47469d3edfd0ebe6cdf8cfd5044
duongphan8b970c-hash <duong.phan8b970c@gmail.com>
2026-03-11 06:10:48 +0000

    Update utils

--- src/pages/finance/WalletConfig.jsx
+++ src/pages/finance/WalletConfig.jsx
@@ -8,7 +8,7 @@ import PageHeader from '../../components/layout/PageHeader'
 import Loading from '../../components/common/Loading'
 import ErrorMessage from '../../components/common/ErrorMessage'
 import { WALLET_TYPES, getWalletTypeInfo } from '../../constants' 
-import { formatNumber } from '../../utils/format'
+import { formatNumber } from '../../utils'
 
 export default function WalletConfig() {
   const { 
